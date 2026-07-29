27edebdfade50f26a5780a8e58d14a6c24b50b1c
anirudhagorai <anirudhagorai123@gmail.com>
2026-07-29 13:33:12 +0530

    Update main

--- src/main.jsx
+++ src/main.jsx
@@ -22,7 +22,7 @@ import { BrowserRouter } from "react-router-dom";
 
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/Portfolio-Website">
     <App />
   </BrowserRouter>
 );
